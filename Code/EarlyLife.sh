eb969a8fc2de67bad5aa6318f4f1959d81cf8fac
jiadeyu0602 <72790855+jiadeyu0602@users.noreply.github.com>
2022-10-18 20:51:11 +0800

    Add files via upload

--- Code/EarlyLife.sh
+++ Code/EarlyLife.sh
@@ -11,7 +11,7 @@
 # load the environment
 module purge
 
-CUDA_LAUNCH_BLOCKING=1 python3 MasterEq.py --L 3 --M 100 --Model 'homo1' --net 'rnn' --lossType 'kl' --max_stepAll 5000 --max_stepLater 100 --lr 0.001 --net_depth 1 --net_width 32 --print_step 1000 --batch_size 1000 --Tstep 20001--delta_t 0.0004 --cuda 0 --dtype float64
+CUDA_LAUNCH_BLOCKING=1 python3 MasterEq.py --L 3 --M 100 --Model 'homo1' --net 'rnn' --lossType 'kl' --max_stepAll 5000 --max_stepLater 100 --lr 0.001 --net_depth 1 --net_width 32 --print_step 1000 --batch_size 1000 --Tstep 20001 --delta_t 0.0004 --cuda 0 --dtype float64
 
 #exit 0
 
