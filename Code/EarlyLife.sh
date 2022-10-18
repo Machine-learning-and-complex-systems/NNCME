b158b2fb9e19e6481f47fcdcd6d2a48b0f1d425c
jiadeyu0602 <72790855+jiadeyu0602@users.noreply.github.com>
2022-10-18 20:55:58 +0800

    Update EarlyLife.sh

--- Code/EarlyLife.sh
+++ Code/EarlyLife.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=homo1-rnn-1-32-M100-Tstep20001-deltaT0.0004
+#SBATCH --job-name=EarlyLife-rnn-1-32-M100-Tstep20001-deltaT0.0004
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=v100
@@ -11,7 +11,7 @@
 # load the environment
 module purge
 
-CUDA_LAUNCH_BLOCKING=1 python3 MasterEq.py --L 3 --M 100 --Model 'homo1' --net 'rnn' --lossType 'kl' --max_stepAll 5000 --max_stepLater 100 --lr 0.001 --net_depth 1 --net_width 32 --print_step 1000 --batch_size 1000 --Tstep 20001 --delta_t 0.0004 --cuda 0 --dtype float64
+CUDA_LAUNCH_BLOCKING=1 python3 MasterEq.py --L 3 --M 100 --Model 'EarlyLife' --net 'rnn' --lossType 'kl' --max_stepAll 5000 --max_stepLater 100 --lr 0.001 --net_depth 1 --net_width 32 --print_step 1000 --batch_size 1000 --Tstep 20001 --delta_t 0.0004 --cuda 0 --dtype float64
 
 #exit 0
 
